91f33cd7541020fd4cef2bba91cfea17c0d1a368
meveo.admin <no-mail@meveo.com>
2022-01-07 00:20:09 +0000

    Add the script File : org.meveo.matrices.MatriceHandlingin the module : matrices

---
+++ facets/java/org/meveo/matrices/MatriceHandling.java
@@ -0,0 +1,15 @@
+package org.meveo.matrices;
+
+import java.util.Map;
+
+import org.meveo.service.script.Script;
+import org.meveo.admin.exception.BusinessException;
+
+public class MatriceHandling extends Script {
+	
+	@Override
+	public void execute(Map<String, Object> parameters) throws BusinessException {
+		super.execute(parameters);
+	}
+	
+}
